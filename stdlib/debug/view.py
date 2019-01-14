fef0375c3670b8ecccd85d9c0298b06a0ddf7a1c
Michael Fürst <mail@michaelfuerst.de>
2019-01-14 21:13:28 +0000

    Fixed debug view to work with current cv and python versions.

--- stdlib/debug/view.py
+++ stdlib/debug/view.py
@@ -6,6 +6,7 @@ This module renders a debug view.
 
 import json
 import sys
+import time
 try:
     import base64
     import numpy as np
@@ -14,13 +15,18 @@ except:
     pass # Well there cannot be any cv input, so there will not occur any error.
 
 def init(node, global_state):
+    node["last_transmission"] = 0
     def tick(value):
         result = {"result": value["val"]}
         if not "debugger" in global_state.shared_dict:
             return result
+
+        now = time.time()
+        if now - node["last_transmission"] < 1.0 / node["args"]["max_fps"]:
+            return result
         
         data_str = ""
-        if type(value["val"]) is list or type(value["val"]) is dict or type(value["val"]) is str or type(value["val"]) is byte or type(value["val"]) is int:
+        if type(value["val"]) is list or type(value["val"]) is dict or type(value["val"]) is str or type(value["val"]) is int:
             data_str = "json:" + json.dumps(value["val"])
         elif type(value["val"]) == np.ndarray and (len(value["val"].shape) != 2 or len(value["val"][1]) != 3) and not (len(value["val"].shape) == 3 and value["val"].shape[2] == 3):
             data_str = "json:" + json.dumps(value["val"].tolist())
@@ -33,12 +39,13 @@ def init(node, global_state):
             if height == 0:
                 height = int(width * 3 / 4)
             img = cv2.resize(img.copy(), (width, height), 0, 0, cv2.INTER_CUBIC)
-            cnt = cv2.imencode('.png', img)[1]
-            b64 = base64.encodestring(cnt)
-            data_str = ("img:" + b64).replace("\n", "")
+            cnt = cv2.imencode('.png', img)[1].tostring()
+            b64 = str(base64.encodestring(cnt))[2:-1]
+            data_str = ("img:" + b64).replace("\n", "").replace("\\n", "")
         else:
             data_str = "type:" + str(type(value["val"]))
         global_state.shared_dict["debugger"].send("data_" + node["node_uid"] + ":" + data_str)
+        node["last_transmission"] = now
         
         return result
 
@@ -50,4 +57,5 @@ def spec(node):
     node["outputs"]["result"] = "Object"
     node["args"]["width"] = 0
     node["args"]["height"] = 0
+    node["args"]["max_fps"] = 2
     node["desc"] = "Views and passes the object."
